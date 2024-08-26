8c82002be10c8c3f7d63775aa94f28910e7dabe9
renatojp15 <renatoj.p@outlook.com>
2024-08-26 11:36:31 -0500

    actualizando archivos HTML a motor de plantillas ejs, hoja de estilos modificada

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+let loginRouter = require('./routes/login');
+let registerRouter = require('./routes/register');
+let productCartRouter = require('./routes/productCart');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use('/productCart', productCartRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
