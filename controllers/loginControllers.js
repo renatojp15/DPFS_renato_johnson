5715a1806e6380b647f7cd8f002599b8699dfde8
renatojp15 <renatoj.p@outlook.com>
2024-10-01 05:01:47 -0500

    modificando el archivo loginControllers.js

--- controllers/loginControllers.js
+++ controllers/loginControllers.js
@@ -1,3 +1,7 @@
+let db = require('../database/models');
+const bcrypt = require('bcryptjs');  
+const { validationResult } = require('express-validator');
+
 let loginControllers = {
 
     login: (req, res) => {
@@ -5,47 +9,37 @@ let loginControllers = {
     },
 
     send: async (req, res) => {
-        let db = require('../database/models');
-        const bcrypt = require('bcryptjs');  
-        const { validationResult } = require('express-validator');
         const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 return res.render('login', { errors: errors.array() });
-            }
-    
-        try {
-            const { email, password } = req.body;
-    
-            // Buscar el usuario en la base de datos por su email
-            const user = await db.Usuario.findOne({
-                where: { email }
+            }  
+            try {
+                const { email, password } = req.body;
+                // Buscar el usuario en la base de datos por su email
+                const user = await db.Usuario.findOne({
+                    where: { email }
             });
-    
-            if (!user) {
-                return res.status(401).send('Usuario no encontrado o contraseña incorrecta');
-            }
-    
-            // Verificar la contraseña usando bcrypt
-            const isMatch = await bcrypt.compare(password, user.password);
-    
-            if (!isMatch) {
-                return res.status(401).send('Usuario no encontrado o contraseña incorrecta');
-            }
-    
-            // Guardar el usuario en la sesión
-            req.session.user = {
-                id: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-                category: user.category,
-                image: user.image
-            };
-    
-            res.redirect('/login/profile'); // Redirigir al usuario a su dashboard o página principal después de loguearse
-        } catch (error) {
-            console.error('Error durante el proceso de login:', error);
-            res.status(500).send('Hubo un problema durante el proceso de login');
+                if (!user) {
+                    return res.status(401).send('Usuario no encontrado');
+                }
+                // Verificar la contraseña usando bcrypt
+                const isMatch = await bcrypt.compare(password, user.password);    
+                if (!isMatch) {
+                    return res.status(401).send('Contraseña incorrecta');
+                }
+                // Guardar el usuario en la sesión
+                req.session.user = {
+                    id: user.id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    category: user.category,
+                    image: user.image
+                };
+                res.redirect('/login/profile'); // Redirigir al usuario a su dashboard o página principal después de loguearse
+        }   catch (error) {
+                console.error('Error durante el proceso de login:', error);
+                res.status(500).send('Hubo un problema durante el proceso de login');
         }
     },
 
